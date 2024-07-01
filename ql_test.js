61546bc017252f432ffcdd15eeb5c79df0a58ca7
liangj <icesword28@qq.com>
2024-07-01 10:36:25 +0800

    Update ql_test.js

--- ql_test.js
+++ ql_test.js
@@ -10,7 +10,7 @@ const notify = require('./notify');
 const $ = new Env('测试');
 
 !(async () => {
-  await notify.telegram($.name, text);
+  await notify.telegram($.name, '你好吗？？？');
 })().catch((e) => {
   $.logErr(e);
 }).finally(() => {
