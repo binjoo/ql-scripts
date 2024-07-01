0515755678f34112bd2ec5d553bf5fc91e0d0e64
liangj <icesword28@qq.com>
2024-07-01 10:34:18 +0800

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,9 +1,13 @@
 async function telegram (title, content) {
   return new Promise((resolve, reject) => {
     const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
+    console.log('TG_BOT_TOKEN', TG_BOT_TOKEN)
     const TG_USER_ID = process.env.TG_USER_ID;
+    console.log('TG_USER_ID', TG_USER_ID)
     const TG_API_HOST = process.env.TG_API_HOST || 'https://api.telegram.org';
+    console.log('TG_API_HOST', TG_API_HOST)
     if (!TG_BOT_TOKEN || !TG_USER_ID) {
+      console.log('!TG_BOT_TOKEN || !TG_USER_ID')
       resolve();
       return;
     }
