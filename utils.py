b8042dc0ffc6b8aea06c1d56ef440da10454beec
binjoo <icesword28@qq.com>
2025-09-04 20:21:07 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,10 +30,43 @@ def push(title: str, content: str):
         response = requests.post(API_URL, headers=headers, json=payload)
         response.raise_for_status()
         log.success(f"通知发送完成，响应状态码 {response.status_code}")
+    except Exception as e:
+        log.error(f"异常类型: {type(e).__name__}")
+        log.error(f"异常信息: {e}")
 
-        try:
-            log.success("服务器响应：", response.json())
-        except json.JSONDecodeError:
-            log.warn("服务器响应：", response.text)
-    except requests.exceptions.RequestException as e:
-        log.error(f"通知发送失败，{e}")
+
+def ai(system: str, content: str) -> str:
+    AI_TOKEN = os.getenv("UTILS_AI_TOKEN")
+    AI_MODEL = os.getenv("UTILS_AI_MODEL")
+    if not AI_TOKEN or not AI_MODEL:
+        log.warn("AI 请求失败，UTILS_AI_TOKEN 或 UTILS_AI_MODEL 未配置。")
+        return None
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AI_TOKEN}",
+    }
+    payload = {
+        "model": AI_MODEL,
+        "messages": [
+            {"role": "system", "content": system},
+            {"role": "user", "content": content},
+        ],
+        "stream": False,
+    }
+
+    try:
+        response = requests.post(
+            "https://api.siliconflow.cn/v1/chat/completions",
+            headers=headers,
+            json=payload,
+            timeout=32,
+        )
+        response.raise_for_status()
+        result = response.json()
+
+        return result["choices"][0]["message"]["content"]
+    except Exception as e:
+        log.error(f"异常类型: {type(e).__name__}")
+        log.error(f"异常信息: {e}")
+        return None
