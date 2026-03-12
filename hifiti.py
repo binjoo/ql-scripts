28440f5c011134c80f912ae7eccfe62ff50d80bb
binjoo <icesword28@qq.com>
2026-03-12 14:28:53 +0800

    Update hifiti.py

--- hifiti.py
+++ hifiti.py
@@ -20,34 +20,34 @@ BASE_URL = "https://www.hifiti.com"
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0"
 )
+HEADERS = {"User-Agent": USER_AGENT, "Content-Type": "application/json;charset=UTF-8"}
 
 PROXIES = {
     "http": os.getenv("PROXY_HTTP"),
     "https": os.getenv("PROXY_HTTPS"),
 }
 
+session = requests.Session()
+
 def login():
     loginUrl = f"{BASE_URL}/user-login.htm"
-    session = requests.Session()
 
-    resp = session.get(loginUrl)
-    log.debug(f"登录页Cookie：{session.cookies.get_dict()}")
+    # resp = session.get(loginUrl)
+    # log.debug(f"登录页Cookie：{session.cookies.get_dict()}")
 
     # 准备登录请求头
-    headers = {
+    addHeaders = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Origin": BASE_URL,
         "Referer": loginUrl,
-        "User-Agent": USER_AGENT,
         "X-Requested-With": "XMLHttpRequest",
         "Accept": "text/plain, */*; q=0.01",
     }
-
     # 表单数据
     data = {"email": USERNAME, "password": utils.md5(PASSWORD)}
 
     # 发送登录 POST 请求
-    response = session.post(loginUrl, headers=headers, data=data, proxies=PROXIES)
+    response = session.post(loginUrl, headers={**HEADERS, **addHeaders}, data=data, proxies=PROXIES)
 
     log.info("尝试登录....")
 
@@ -55,27 +55,26 @@ def login():
         result = response.json()
         if result.get("code") == "0":
             log.success("登录成功")
-            return session
+            return True
         else:
             log.error(f"登录失败：{result.get('message')}")
             utils.push("HiFiTi 签到", f"登录失败：{result.get('message')}")
-            return None
+            return False
     except json.JSONDecodeError:
         log.warn("返回内容不是有效 JSON，可能登录失败或者返回了登录页面HTML")
         return None
 
 
-def sign(session):
+def sign():
     signUrl = f"{BASE_URL}/sg_sign.htm"
-    headers = {
+    addHeaders = {
         "X-Requested-With": "XMLHttpRequest",
         "Origin": BASE_URL,
         "Referer": f"{BASE_URL}/",
-        "User-Agent": USER_AGENT,
         "Accept": "text/plain, */*; q=0.01",
     }
 
-    response = session.post(signUrl, headers=headers, data={}, proxies=PROXIES)
+    response = session.post(signUrl, headers={**HEADERS, **addHeaders}, data={}, proxies=PROXIES)
 
     try:
         result = response.json()
@@ -88,23 +87,21 @@ def sign(session):
         print(response.text)
 
 
-def getCredits(session):
+def getCredits():
     myUrl = f"{BASE_URL}/my.htm"
-    headers = {
-        "User-Agent": USER_AGENT,
+    addHeaders = {
         "Referer": f"{BASE_URL}/sg_sign.htm",
     }
 
-    response = session.get(myUrl, headers=headers, proxies=PROXIES)
+    response = session.get(myUrl, headers={**HEADERS, **addHeaders}, proxies=PROXIES)
     if response.status_code != 200:
         log.error(f"请求失败，状态码：{response.status_code}")
         return None
 
     soup = BeautifulSoup(response.text, "html.parser")
-
-    span = soup.find("span", string="金币：")
+    span = soup.find("span", class_="text-muted", text="金币：")
     if span:
-        em = span.find_next_sibling("b")
+        em = span.find_next_sibling("em")
         if em:
             return em.text.strip()
 
@@ -120,10 +117,10 @@ if __name__ == "__main__":
         )
         exit(1)
 
-    session = login()
-    if session:
-        sign(session)
-        gold = getCredits(session)
+    loginCon = login()
+    if loginCon:
+        sign()
+        gold = getCredits()
         if gold is not None:
             log.success(f"当前金币数量：{gold}", "🪙")
         else:
